bd1e913b254916e3e5a8f23d94fb3e0ebff3b6d6
olesyamogil <olesya.mogil@gmail.com>
2019-12-15 13:07:44 +0200

    Updates

---
+++ precache-manifest.041f42727cd379de028bb0d3cdfbd826.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9d17e210b7fa46a78bbcff8f6ef86a35",
+    "url": "/books/index.html"
+  },
+  {
+    "revision": "dcc2d11d82e9afcb539a",
+    "url": "/books/static/css/main.4f5f92ed.chunk.css"
+  },
+  {
+    "revision": "fb286bbb3c22d97700b1",
+    "url": "/books/static/js/2.ec6b7608.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/books/static/js/2.ec6b7608.chunk.js.LICENSE"
+  },
+  {
+    "revision": "dcc2d11d82e9afcb539a",
+    "url": "/books/static/js/main.971b12f3.chunk.js"
+  },
+  {
+    "revision": "e19ba1889b05f4c6be5f",
+    "url": "/books/static/js/runtime-main.74d44683.js"
+  }
+]);
